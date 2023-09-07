d838dec3ea8872fef22311be15fde6c19a0c49b4
Казакова Елизавета Вячеславовна <e.kazakova3@g.nsu.ru>
2023-09-07 14:21:48 +0700

    add thymeleaf

--- project/src/main/java/ru/nsu/fit/ekazakova/cityPhiharmonic/repository/organizer/OrganizerCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.fit.ekazakova.cityPhiharmonic.repository.organizer;
-
-import java.time.LocalDate;
-import java.util.List;
-
-public interface OrganizerCustomRepository {
-
-    List<OrganizerDetails> findOrganizersInPeriod(LocalDate startDate, LocalDate endDate);
-}
