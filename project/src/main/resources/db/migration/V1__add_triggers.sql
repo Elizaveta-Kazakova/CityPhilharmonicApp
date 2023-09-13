c4a6f805346ee20543818240d9e2fdd69e58036f
Казакова Елизавета Вячеславовна <e.kazakova3@g.nsu.ru>
2023-09-13 16:54:19 +0700

    add flyway migrations

--- project/src/main/resources/db/migration/V1__add_triggers.sql
+++ project/src/main/resources/db/migration/V1__add_triggers.sql
@@ -1,17 +1,17 @@
 create function check_event_date() RETURNS trigger AS $$
-    declare
-        x integer = 0;
-    begin
-        select count(*)
-        into x
-        from event
-        where (new.date = event.date and new.cultural_building_id = event.cultural_building_id);
-        if (x > 0) then
+declare
+x integer = 0;
+begin
+select count(*)
+into x
+from event
+where (new.date = event.date and new.cultural_building_id = event.cultural_building_id);
+if (x > 0) then
             RAISE EXCEPTION 'The event at this time and place already exists.'
                 USING HINT = 'Please choose a different time or cultural building';
-        end if;
-        RETURN new;
-    end;
+end if;
+RETURN new;
+end;
 $$ LANGUAGE plpgsql;
 
 DROP TRIGGER IF EXISTS is_such_event_exist
@@ -27,18 +27,18 @@ end;
 -- артисты не могут быть одновременно на двух мероприятиях в одну и ту же дату
 create function check_artist_events() RETURNS trigger AS $$
 declare
-    x integer = 0;
+x integer = 0;
 begin
-    select count(*)
-    into x
-    from event_artist
-    left join event on event.id = event_artist.event_id left join event e on e.id = new.event_id
-    where event_artist.artist_id = new.artist_id and e.id = new.event_id and event.date = e.date;
-    if (x > 0) then
+select count(*)
+into x
+from event_artist
+         left join event on event.id = event_artist.event_id left join event e on e.id = new.event_id
+where event_artist.artist_id = new.artist_id and e.id = new.event_id and event.date = e.date;
+if (x > 0) then
         RAISE EXCEPTION 'This artist is already busy on this date.'
             USING HINT = 'Please choose a different time or artist';
-    end if;
-    RETURN new;
+end if;
+RETURN new;
 end;
 $$ LANGUAGE plpgsql;
 
@@ -54,19 +54,19 @@ end;
 -------------------------------------------------------------------
 
 create function check_organizer_events() RETURNS trigger AS $$
-    declare
-        x integer = 0;
-    begin
-        select count(*)
-        into x
-        from event
-        where (new.date = event.date and new.organizer_id = event.organizer_id);
-        if (x > 0) then
+declare
+x integer = 0;
+begin
+select count(*)
+into x
+from event
+where (new.date = event.date and new.organizer_id = event.organizer_id);
+if (x > 0) then
             RAISE EXCEPTION 'The organizer is already organizing another event on this day.'
                 USING HINT = 'Please choose a different time or organizer';
-        end if;
-        RETURN new;
-    end;
+end if;
+RETURN new;
+end;
 $$ LANGUAGE plpgsql;
 
 DROP TRIGGER IF EXISTS is_organizer_busy
@@ -81,18 +81,18 @@ end;
 -------------------------------------------------------------------
 -- занять одно и то же место на одном конкурсе два разных артиста не могут
 create function check_winner_places() RETURNS trigger AS $$
-    declare
-        x integer = 0;
-    begin
-        select count(*)
-        into x
-        from competition_artist
-        where (new.competition_id = competition_artist.competition_id and new.place_in_rating = competition_artist.place_in_rating);
-        if (x > 0) then
+declare
+x integer = 0;
+begin
+select count(*)
+into x
+from competition_artist
+where (new.competition_id = competition_artist.competition_id and new.place_in_rating = competition_artist.place_in_rating);
+if (x > 0) then
             RAISE EXCEPTION 'This place is already occupied by another artist.';
-        end if;
-        RETURN new;
-    end;
+end if;
+RETURN new;
+end;
 $$ LANGUAGE plpgsql;
 
 DROP TRIGGER IF EXISTS is_place_occupied
@@ -108,17 +108,17 @@ end;
 -- конкурс проходит на мероприятии, на котором нет этого артиста
 create function check_competition_event() RETURNS trigger AS $$
 declare
-    x integer = 0;
+x integer = 0;
 begin
-    select count(*)
-    into x
-    from competition c left join event e on e.id = c.event_id
-                       left join event_artist ea on ea.event_id = e.id
-    where ea.artist_id = new.artist_id and c.id = new.competition_id;
-    if (x = 0) then
+select count(*)
+into x
+from competition c left join event e on e.id = c.event_id
+                   left join event_artist ea on ea.event_id = e.id
+where ea.artist_id = new.artist_id and c.id = new.competition_id;
+if (x = 0) then
         RAISE EXCEPTION 'The competition takes place at an event where this artist is not present';
-    end if;
-    RETURN new;
+end if;
+RETURN new;
 end;
 $$ LANGUAGE plpgsql;
 
@@ -128,5 +128,5 @@ DROP TRIGGER IF EXISTS is_competition_from_another_event
 create trigger is_competition_from_another_event
     before insert on competition_artist
     for each row
-execute function check_competition_event();
+    execute function check_competition_event();
 end;
